c3adab4e2b97a78658a264725d64be7b2af23720
olga <okoteogl@gmail.com>
2013-04-15 22:49:52 -0400

    Parser fixed and viewer added
    1. .picasa.ini fixed
    2. viewer to show images and the rectangles of the recognized faces

---
+++ src/ImageViewer.py
@@ -0,0 +1,65 @@
+'''
+Created on Apr 2, 2013
+
+@author: olga
+'''
+
+from PIL import Image, ImageTk
+import Tkinter
+import os
+
+def onRightArrowPress(event):
+  global filename_index
+  filename_index += 1
+  event.widget.quit()  # exit mainloop and go to the next image
+    
+def onLeftArrowPress(event):
+  global filename_index
+  filename_index -= 1
+  event.widget.quit()  # exit mainloop and go to the previous image
+
+def runViewer(mypath, myTags):
+  global filename_index
+  dirlist = os.listdir(mypath)
+
+  for item in dirlist[:]:
+    if not item.endswith("png" or "jpg" or "jpeg"):  # whatever test you need to run goes here
+      dirlist.remove(item)
+  
+  filename_index = 0
+  root = Tkinter.Tk()
+  frame_temp = Image.open(os.path.join(mypath, dirlist[0]))
+  img_width, img_height = frame_temp.size   
+  canvas = Tkinter.Canvas(root, width=img_width, height=img_height)
+  canvas.pack()
+  root.bind("<Right>", onRightArrowPress)
+  root.bind("<Left>", onLeftArrowPress)
+  root.geometry('+%d+%d' % (100, 100))
+
+  while True:
+    try:
+      canvas.delete("all")
+      image1 = Image.open(os.path.join(mypath, dirlist[filename_index]))
+      root.geometry('%dx%d' % (image1.size[0], image1.size[1]))
+      
+      tkpi = ImageTk.PhotoImage(image1)
+      canvas.create_image((img_width / 2, img_height / 2), image=tkpi)     
+      root.title(dirlist[filename_index])
+      
+      if dirlist[filename_index] in myTags:
+        for tag in myTags[dirlist[filename_index]]:
+          coords = tag[1]
+          print coords
+          canvas.create_rectangle(coords, fill=None, outline="red")     
+
+      root.mainloop()  # wait until user clicks the window
+    except:
+      root.mainloop()
+    try:
+      Tkinter.Label(root).pack()
+    except:
+      print "window has been destroyed, break loop"
+      break
+
+if __name__ == '__main__':
+  runViewer()
--- src/PicasaIniParser.py
+++ src/PicasaIniParser.py
@@ -4,27 +4,44 @@ Created on Mar 30, 2013
 @author: olga
 '''
 
-from os import listdir
-from os.path import isfile, join
-from PIL import Image, ImageDraw
+import ImageViewer
+# from os import listdir
+# from os.path import isfile, join
+from PIL import Image  # , ImageDraw
 import re
 
-picasainiFile = open("C:/Users/olga/Downloads/Annarella/video_frames/.picasa.ini")
-mypath = "C:/Users/olga/Downloads/Annarella/video_frames/"
-onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith(".png" or ".jpg" or "jpeg")]
-# print onlyfiles
+def appendInTags(frame, identity, coords):
+  global myPicasaIniTags
+  if frame in myPicasaIniTags:
+    myPicasaIniTags[frame].append((identity, coords))
+  else:
+    myPicasaIniTags[frame] = [(identity, coords)]
+  # print myPicasaIniTags
 
-p1 = re.compile("\_[0-9]*\.")
-p2 = re.compile("\([0-9a-z]+\)")
-p3 = re.compile("\,[0-9a-z]+[\;|\n]")
-line_number = 1
-for line in picasainiFile:
+
+def readPicasaIniFile():
   
-  if line_number == 1:
-    line_number = 2
-    filename = str(line[1:len(line) - 2])
-    print filename
-    continue
+  # ImageViewer2.runViewer("C:/Users/olga/Downloads/Annarella/video_frames")
+  global mypath
+  picasainiFile = open("C:/Users/olga/Downloads/Annarella2/video_frames/.picasa.ini")
+  mypath = "C:/Users/olga/Downloads/Annarella2/video_frames/"
+  # onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith(".png" or ".jpg" or "jpeg")]
+  # print onlyfiles
+
+  # p1 = re.compile("\_[0-9]*\.")
+  p2 = re.compile("\([0-9a-z]+\)")
+  p3 = re.compile("\,[0-9a-z]+[\;|\n]")
+  line_number = 1
+  global myPicasaIniTags
+  myPicasaIniTags = {}
+  for line in picasainiFile:
+  
+    if line_number == 1:
+      # print line_number, line,
+      line_number = 2
+      filename = str(line[1:-2])
+      # print filename
+      continue
 #     list_of_frame_numbers = p1.findall(line)
 #     if list_of_frame_numbers:
 #       frame_number = str(list_of_frame_numbers.pop())
@@ -32,45 +49,54 @@ for line in picasainiFile:
 #       print "Frame number: ", frame_number
     
       
-  if line_number == 2:
-    line_number = 3    
-    list_of_identities = p3.findall(line)
-    if list_of_identities:
-      print "identities"
-      for i in range(0, len(list_of_identities)):
-        identities = str(list_of_identities.pop())
-        identities = identities[1:len(identities) - 1]
-        print identities  # , float.fromhex(identities[0:4])
-    
-      frame = Image.open(mypath + filename)
-      width, height = frame.size
+    if line_number == 2:
+      # print line_number, line,
+      line_number = 3    
+      list_of_identities = p3.findall(line)
       list_of_bounding_boxes = p2.findall(line)
-      if list_of_bounding_boxes:
-        print "bounding boxes:"
-        for i in range(0, len(list_of_bounding_boxes)):
-          bounding_box = str(list_of_bounding_boxes.pop())
-          bounding_box = bounding_box[1:len(bounding_box) - 1]
-          print bounding_box
+      if list_of_identities:
+        # print "identities"
+        for i in range(0, len(list_of_identities)):
+          identity = str(list_of_identities[i])
+          identity = identity[1:-1]
+          # print identity  # , float.fromhex(identities[0:4])
+    
+          frame = Image.open(mypath + filename)
+          width, height = frame.size          
+          # if list_of_bounding_boxes:
+          # print "bounding boxes:"
+            # for j in list_of_bounding_boxes:
+          bounding_box = str(list_of_bounding_boxes[i])
+          bounding_box = bounding_box[1:-1]
+          if len(bounding_box) < 16:
+            continue
+          # print bounding_box
           coords = []
           for i in range(0, 4):
             coords.append(float(int(bounding_box[i * 4:(i + 1) * 4], 16)) / 65535)
-            print int(bounding_box[i * 4:(i + 1) * 4 ], 16)
+            # print int(bounding_box[i * 4:(i + 1) * 4 ], 16)
           coords[0] = coords[0] * width
           coords[2] = coords[2] * width
           coords[1] = coords[1] * height
           coords[3] = coords[3] * height
-          print coords
-      draw = ImageDraw.Draw(frame)
-      draw.rectangle((coords[0], coords[1], coords[2], coords[3]), fill=None, outline="red")
-      frame.show()
-    continue      
+          # print coords
+          appendInTags(filename, identity, coords)
+#             draw = ImageDraw.Draw(frame)
+#             draw.rectangle((coords[0], coords[1], coords[2], coords[3]), fill=None, outline="red")
+#             frame.show()
+        continue      
         
-  if line_number == 3:
-    line_number = 1
-    continue
-
+    if line_number == 3:
+      # print line_number, line
+      line_number = 1
+      
+  return myPicasaIniTags
         
-        
+if __name__ == '__main__':
+  global mypath
+  myTags = readPicasaIniFile()
+  print myTags
+  ImageViewer.runViewer(mypath, myTags)
         
         
     
